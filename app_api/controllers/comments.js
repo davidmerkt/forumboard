54a4b4d652b8da952b14e034fb631e921d82199e
davidmerkt <davidmerkt@users.noreply.github.com>
2019-05-20 20:55:26 -0400

    Add CommentsUpdateOne function to API

--- app_api/controllers/comments.js
+++ app_api/controllers/comments.js
@@ -56,9 +56,51 @@ const commentsReadOne = (req, res) => {
 };
 
 const commentsUpdateOne = (req, res) => {
-  res
-    .status(200)
-    .json({status: "success"});
+  const {threadid, commentid} = req.params;
+  if (!threadid || !commentid) {
+    return res
+      .status(404)
+      .json({message: "Not found, threadid and commentid are both required"});
+  }
+  Thread
+    .findById(threadid)
+    .select('comments')
+    .exec((err, thread) => {
+      if (!thread) {
+        return res
+          .status(404)
+          .json({message: "Thread not found"});
+      } else if (err) {
+        return res
+          .status(400)
+          .json(err);
+      }
+      if (thread.comments && thread.comments.length > 0) {
+        const thisComment = thread.comments.id(commentid);
+        if (!thisComment) {
+          res
+            .status(404)
+            .json({message: "Comment not found"});
+        } else {
+          thisComment.commentText = req.body.commentText;
+          thread.save((err, thread) => {
+            if (err) {
+              res
+                .status(400)
+                .json(err);
+            } else {
+              res
+                .status(200)
+                .json(thisComment);
+            }
+          });
+        }
+      } else {
+        res
+          .status(404)
+          .json({message: "No comment to update"});
+      }
+    });
 };
 
 const commentsDeleteOne = (req, res) => {
